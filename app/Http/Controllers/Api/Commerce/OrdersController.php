52bfc52520c80a2de0d84e7823d9a04b8b33e639
DenoGeek <kariukidennisde@gmail.com>
2019-05-11 18:13:55 +0300

    the updates to order endpoint

--- app/Http/Controllers/Api/Commerce/OrdersController.php
+++ app/Http/Controllers/Api/Commerce/OrdersController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\Api\Commerce;
 
+use App\Jobs\NotifyRidersForOrder;
+use App\Models\Order;
+use App\Models\OrderItems;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Webpatser\Uuid\Uuid;
 
 class OrdersController extends Controller
 {
@@ -14,10 +20,52 @@ class OrdersController extends Controller
 
         $data = json_decode($request->getContent());
 
-        Log::warning(json_encode($data));
+        /*
+         * Create an order record
+         */
+        $order = new Order();
+        $order->user_id = Auth::id();
+        $order->id=Uuid::generate();
+        $order->location = $data->location;
+        $order->phone=$data->phone;
+        $order->order_no=$this->generateOrderNo();
+        $order->save();
+
+        foreach ($data->products_in_cart as $item){
+            $order_item=[
+                'product_id'=>$item->id,
+                'qty'=>$item->quantity,
+                'order_id'=>$order->id,
+            ];
+            OrderItems::create($order_item);
+        }
+
+        $this->dispatch(new NotifyRidersForOrder($order));
 
         return response()->json([
             'success'=>true
         ]);
+    }
+
+
+    function generateOrderNo(){
+        $pin = $this->generatePIN();
+        $validator=Validator::make(['order_no'=>$pin],['order_no'=>'unique:orders,order_no']);
+        if($validator->fails()){
+            return $this->generateOrderNo();
+        }else{
+            return $pin;
+        }
+    }
+
+    function generatePIN($digits = 4){
+        $i = 0; //counter
+        $pin = ""; //our default pin is blank.
+        while($i < $digits){
+            //generate a random number between 0 and 9.
+            $pin .= mt_rand(0, 9);
+            $i++;
+        }
+        return $pin;
     }
 }
---
+++ database/migrations/2019_05_11_142237_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('order_no');
+            $table->unsignedBigInteger('user_id');
+            $table->string('location');
+            $table->string('phone')->nullable();
+            $table->integer('status')->default(0); //0-Fresh order
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
