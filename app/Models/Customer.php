5cd1ab04cc3acbd21464ef29f485dc19b344a048
DenoGeek <kariukidennisde@gmail.com>
2019-05-14 15:04:26 +0300

    the fix

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\CreateOrUpdateExcept;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
     //
+    use CreateOrUpdateExcept;
+
+
     public function customer()
     {
         return $this->hasOne(Customer::class,'user_id','id');
