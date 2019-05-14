b163c906c1e5398866c0d55d70b7d043d319d888
DenoGeek <kariukidennisde@gmail.com>
2019-05-14 14:59:00 +0300

    the fix of fixes

--- app/Http/Controllers/Api/Commerce/CustomerController.php
+++ app/Http/Controllers/Api/Commerce/CustomerController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api\Commerce;
 
+use App\Models\Customer;
+use App\Models\Rider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Webpatser\Uuid\Uuid;
 
 class CustomerController extends Controller
 {
@@ -14,6 +17,14 @@ class CustomerController extends Controller
         $user = Auth::user();
         $user->login_mode = 0;
         $user->save();
+
+        /*
+         * create customer record
+         */
+        $rider = ["user_id"=>Auth::id(),"id"=>Uuid::generate()];
+        Customer::createOrUpdateExcept(['user_id'=>Auth::id()],$rider,['id']);
+
+
         return response()->json($user->customer);
     }
 
